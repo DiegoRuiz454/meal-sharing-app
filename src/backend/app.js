78aa6f907d2c79b3ce284e76a6d8e3a6877a3b73
DiegoRuiz <differ454@hotmai.com>
2021-01-13 14:09:25 +0100

    backend changes implemented

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const path = require("path");
-
+const mime = require('mime');
 
 const mealsRouter = require("./api/meals");
 const reservationsRouter = require("./api/reservations");
@@ -11,18 +11,26 @@ const reservationsRouter = require("./api/reservations");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const setHeadersOnStatic = (res, path, stat) => {
+const type = mime.getType(path);
+res.set('content-type', type);
+}
+
+const staticOptions = {
+  setHeaders: setHeadersOnStatic
+}
 
 
+// this path is to show correctly de imagen and files into heroku
+
+app.use(express.static(path.join(__dirname, 'public'), staticOptions));
 
 // For week4 no need to look into this!
 // Serve the built client html
 app.use(express.static(buildPath));
   
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(
-  express.urlencoded({
-     extended: true,
-     }));
+app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());   
 
